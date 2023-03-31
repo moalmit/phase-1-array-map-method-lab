6e6f23f6ff2c1ad568cba2247728edcc0a895b29
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-03-31 01:54:19 -0700

    complete assignment

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+
+  const x = tutorials.map((element => element.split(" ")));
+
+// console.log(x);
+
+  for (let str of x){
+
+    for (let i=0;i<str.length;i++){
+      str[i] = (str[i].charAt(0).toUpperCase() + str[i].slice(1));
+      }
+   
+    }
+// console.log(x);
+
+let y = x.map((element) => element.join(" "));
+
+// console.log (y);
+
+
+
+return y
+
 }
